41439bbf7585bc99749524fe33e2d245e236fe5a
caichuibin <caichuibin@baidu.com>
2016-08-28 15:50:13 +0800

    frist commit

---
+++ background.js
@@ -0,0 +1,23 @@
+﻿function getDomainFromUrl(url){
+	var host = "null";
+	if(typeof url == "undefined" || null == url)
+		url = window.location.href;
+	var regex = /.*\:\/\/([^\/]*).*/;
+	var match = url.match(regex);
+	if(typeof match != "undefined" && null != match)
+		host = match[1];
+	return host;
+}
+
+function checkForValidUrl(tabId, changeInfo, tab) {
+    var url = getDomainFromUrl(tab.url).toLowerCase();
+    
+	//if(url=="www.cnblogs.com"){
+		chrome.pageAction.show(tabId);
+	//}
+    if(url=="www.google.com"){
+        console.log("谷歌，我来了");
+    }
+};
+
+chrome.tabs.onUpdated.addListener(checkForValidUrl);
---
+++ content_script.js
@@ -0,0 +1,20 @@
+﻿console.log("get google search page content, 3.3V");
+
+console.log($('#ires'));
+
+$('#ires').css({'background':'pink'});  
+$('#ires a').each(function(index) {
+ //$(this).attr('href', $(this).attr('data-href'));
+ //$(this).attr('href', '#');
+ //$(this).attr('onmousedown', 'console.log("onmousedown"); return GSOnMouseDown(this); ');
+ console.log(index + '.  '+$(this).attr('href'));
+ //console.log(index + '.  '+$(this).attr('data-href'));
+});
+
+/*function GSOnMouseDown(obj){
+    //console.log('GSOnMouseDown:' + obj);
+    //$(obj).attr()
+    return false;
+}*/
+
+console.log('content_script run end');
